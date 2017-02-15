fdada1145323c17e54134eb75a91e4e3fb3c3e9b
DennisGuo <guohengxi.dennis@gmail.com>
2017-02-15 15:45:10 +0800

    update js to closure.

--- public/js/response.js
+++ public/js/response.js
@@ -1,30 +1,30 @@
-(function () {
+(function (document) {
 
     var nsiehost;
     var nsieversion;
     var nsieinfo;
 
-    var createStylesheet = function(){
+    var createStylesheet = function () {
         var link = document.createElement("link");
         link.href = nsiehost + "/css/not-support-ie.css";
         link.rel = "stylesheet";
         document.getElementsByTagName('head')[0].appendChild(link);
     };
 
-    var createOverflow = function(){
+    var createOverflow = function () {
         var div = document.createElement("div");
         div.id = "nsie-overflow"
         div.className = "nsie-overflow";
         document.body.appendChild(div);
     };
 
-    var createAlert = function(){
+    var createAlert = function () {
         var div = document.createElement("div");
         div.id = "nsie-alert"
-        div.className ="nsie-alert";
+        div.className = "nsie-alert";
 
         var h = document.createElement('h2');
-        h.innerText = "Sorry, We're Not Supporting IE"+nsieversion+"- Anymore !"
+        h.innerText = "Sorry, We're Not Supporting IE" + nsieversion + "- Anymore !"
         div.appendChild(h);
 
         var p = document.createElement('p');
@@ -37,18 +37,17 @@
         div.appendChild(p1);
 
         var ul = document.createElement('ul');
-        ul.innerHTML = 
-            "<li><a target='_blank' href='https://www.google.com/chrome/browser/desktop/index.html'/><img src='"+nsiehost+"/images/chrome_256x256.png' /><br/>Google Chrome</a></li>"+
-            "<li><a target='_blank' href='https://www.mozilla.org'/><img src='"+nsiehost+"/images/firefox_256x256.png' /><br/>Firefox</a></li>"+
-            "<li><a target='_blank' href='https://www.microsoft.com/en-us/windows/microsoft-edge'/><img src='"+nsiehost+"/images/edge_256x256.png' /><br/>Edge</a></li>"+
-            "<li><a target='_blank' href='https://www.opera.com'/><img src='"+nsiehost+"/images/opera_256x256.png'/><br/>Firefox</a></li>"
-        ;
+        ul.innerHTML =
+            "<li><a target='_blank' href='https://www.google.com/chrome/browser/desktop/index.html'/><img src='" + nsiehost + "/images/chrome_256x256.png' /><br/>Google Chrome</a></li>" +
+            "<li><a target='_blank' href='https://www.mozilla.org'/><img src='" + nsiehost + "/images/firefox_256x256.png' /><br/>Firefox</a></li>" +
+            "<li><a target='_blank' href='https://www.microsoft.com/en-us/windows/microsoft-edge'/><img src='" + nsiehost + "/images/edge_256x256.png' /><br/>Edge</a></li>" +
+            "<li><a target='_blank' href='https://www.opera.com'/><img src='" + nsiehost + "/images/opera_256x256.png'/><br/>Firefox</a></li>";
         div.appendChild(ul);
 
-         var close = document.createElement('span');
+        var close = document.createElement('span');
         close.className = "nsie-close"
         close.innerText = "close";
-        close.onclick = function(){
+        close.onclick = function () {
             var overflow = document.getElementById('nsie-overflow');
             var alert = document.getElementById('nsie-alert');
             document.body.removeChild(overflow);
@@ -64,4 +63,4 @@
         createOverflow();
         createAlert();
     }, 100)
-}())
+}(window.document))
